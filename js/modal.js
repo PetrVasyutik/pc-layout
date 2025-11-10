11dc9c2289fb86c720b61e2a373b4d4c73578f00
Petr <vasutik@mail.ru>
2025-11-10 14:16:50 +0300

    Добавляет открытие-закрытие модального окна

---
+++ js/modal.js
@@ -0,0 +1,38 @@
+const modal = document.querySelector('.modal');
+const courseButton = document.querySelector('.course__button');
+const assemblyButton = document.querySelector('.assembly__button');
+
+function openModal() {
+  if (modal) {
+    modal.style.display = 'flex';
+  }
+}
+
+function closeModal() {
+  if (modal) {
+    modal.style.display = '';
+  }
+}
+
+if (courseButton) {
+  courseButton.addEventListener('click', openModal);
+}
+
+if (assemblyButton) {
+  assemblyButton.addEventListener('click', openModal);
+}
+
+modal.addEventListener('click', (event) => {
+  const modalContent = event.target.closest('.modal__inner');
+
+  if (!modalContent) {
+    closeModal();
+  }
+});
+
+const button = document.createElement('button');
+button.classList.add('modal__button-close');
+button.textContent = 'X';
+const container = document.querySelector('.modal__inner');
+container.appendChild(button);
+button.addEventListener('click', closeModal);
---
+++ js/smoothScroll.js
@@ -0,0 +1,20 @@
+const navbar = document.querySelector('.header__nav');
+const links = navbar.querySelectorAll('a[href^="#"]');
+
+links.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const section = document.querySelector(link.getAttribute('href'));
+
+    if (section) {
+      seamless.scrollIntoView(section, {
+        behavior: "smooth",
+        block: "start",
+        inline: "center",
+      });
+    }
+  });
+});
+
+
